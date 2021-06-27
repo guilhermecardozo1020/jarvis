7b9ce1edb40e5e494ac6366384dfaa18648bd600
guilhermecardozo1020 <guilherme.cardozo@imed.edu.br>
2021-06-27 19:25:48 -0300

    Criação da tabela de quadro de horários
    Criação manual no Firebase dos horários da jornada de trabalho de cada colaborador, além de popular a tabela no Front com os dados cadastrados.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Search from '../components/Search'
 import Menu from '../components/Menu'
 import ImagemBanner from '../images/banner_colaboradores_background.jpg';
 import Firebase from '../services/FirebaseConnect'
-import {makeStyles } from '@material-ui/core/styles';
+import {makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import ImagemAvatar from '../images/imagem-colaborador.png';
 import List from '@material-ui/core/List';
@@ -13,7 +13,51 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import 'date-fns';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+  
+  function createData(hour, func_um, func_dois, func_tres, func_quatro) {
+    return { hour, func_um, func_dois, func_tres, func_quatro};
+  }
+  
+  const rows = [
+    createData('8h', "Não", "Sim", "Não", "Sim"),
+    createData('9h',  "Não", "Sim", "Não", "Sim"),
+    createData('10h',  "Sim", "Sim", "Não", "Sim"),
+    createData('11h',  "Sim", "Sim", "Não", "Sim"),
+    createData('12h',  "Sim", "Não", "Não", "Sim"),
+    createData('13h', "Não", "Parcial", "Sim", "Parcial"),
+    createData('14h',  "Sim", "Sim", "Sim", "Sim"),
+    createData('15h',  "Sim", "Sim", "Sim", "Sim"),
+    createData('16h',  "Sim", "Sim", "Sim", "Sim"),
+    createData('17h',  "Sim", "Sim", "Sim", "Sim"),
+    createData('18h',  "Sim", "Sim", "Sim", "Sim"),
+  ];
+
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
@@ -41,11 +85,26 @@ const useStyles = makeStyles((theme) => ({
       },
       contentPaperColaboradores: {
           marginBottom: '25px'
+      },
+      table: {
+        maxWidth: '90%',
+        margin: 'auto'
+      },
+      table_head: {
+        color: '#000',
+        backgroundColor: '#bababa'
+      },
+      table_row: {
+          backgroundColor: "#007f00",
+          color: "#fff",
+          textAlign: 'center',
+          width: "10%"
       }
   }));
   
 export default function Colaboradores() {
     const [lista, setLista] = useState([])
+    const [horarios, setHorarios] = useState([])
     useLayoutEffect(() => {
         Firebase    
             .database()
@@ -61,23 +120,78 @@ export default function Colaboradores() {
                     setLista(lista)
                 }
             })
-    }, [])
 
+        Firebase    
+            .database()
+            .ref(`/horarios`)
+            .on('value', snapchot => {
+                //converter objetos em listas
+                if (snapchot.val()) {
+                    let dados = snapchot.val()
+                    const keys = Object.keys(dados)
+                    const lista = keys.map((key) => {
+                        return {...dados[key], id:key}
+                    })
+                    setHorarios(lista)
+                }
+            })
+        
+    }, [])
     const classes = useStyles();
     return (
         <div>
-            <Banner title="Seja Bem Vindo "
-                description="ao Painel da Jornada de Trabalho !"
+            <Banner title="Seja Bem-Vindo "
+                description="ao Painel da Jornada de Trabalho Jarvis!"
                 image={ImagemBanner}
                 imageText="Imagem ilustrativa pessoa usando notebook" />
             <Menu tabAtiva={0}/>
+            <div className={classes.contentColaboradores}>
+            <Typography className={classes.tituloColaboradores} variant="h5" component="h3">                    
+                </Typography>
+                <Typography className={classes.tituloColaboradores} variant="h5" component="h3">           
+                    Jornada de Trabalho Atual         
+                </Typography>
+                <Typography className={classes.tituloColaboradores} variant="h6" component="h6">           
+                    Período: 31/05/2021 a 31/07/2021         
+                </Typography>
+                </div>
+            <TableContainer>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                    <StyledTableCell className={classes.table_row}>Horários</StyledTableCell>
+                    { lista.map((item) => {
+                        return item.statusColaborador == 'Ativo' ? <StyledTableCell className={classes.table_head}>
+                            <ListItemAvatar className={classes.avatarColaborador}>
+                                    <Avatar alt="Remy Sharp" src={ImagemAvatar}  />
+                            </ListItemAvatar>
+                                {item.nome}
+                            </StyledTableCell>:null
+                    })}
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {horarios.map((row) => (
+                            <StyledTableRow>
+                            <StyledTableCell className={classes.table_row} component="th" scope="row">
+                                {row.horario}
+                            </StyledTableCell>
+                            <StyledTableCell align="left">{row.colaborador1}</StyledTableCell>
+                            <StyledTableCell align="left">{row.colaborador2}</StyledTableCell>
+                            <StyledTableCell align="left">{row.colaborador3}</StyledTableCell>
+                            <StyledTableCell align="left">{row.colaborador4}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
+
             <div className={classes.contentColaboradores} >
                 <Typography className={classes.tituloColaboradores} variant="h5" component="h3">                    
                 </Typography>
                 <Typography className={classes.tituloColaboradores} variant="h5" component="h3">           
-                Seus colaboradores          
+                Seus colaboradores ativos         
                 </Typography>
-                <Search></Search>
                 { lista.map((item, key) => {
                     return item.statusColaborador == 'Ativo' ? <Paper className={classes.contentPaperColaboradores} elevation={2}>
                         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
--- src/services/FirebaseConfig.js
+++ src/services/FirebaseConfig.js
@@ -1,11 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyC33fzUUhaol1elhP5PMWDu6yGzgSRCRUg",
-    authDomain: "jarvis-2b36c.firebaseapp.com",
-    databaseURL: "https://jarvis-2b36c-default-rtdb.firebaseio.com",
-    projectId: "jarvis-2b36c",
-    storageBucket: "jarvis-2b36c.appspot.com",
-    messagingSenderId: "81384602882",
-    appId: "1:81384602882:web:2cc3748cca849781b6d61b",
-    measurementId: "G-RPM3P3BFFD"
+    apiKey: "AIzaSyAGxUcFphoTRc0yZmnnbif2IjELxSZJkcI",
+    authDomain: "jarvis-project-final.firebaseapp.com",
+    projectId: "jarvis-project-final",
+    storageBucket: "jarvis-project-final.appspot.com",
+    messagingSenderId: "513220715001",
+    appId: "1:513220715001:web:cd38cb831bb60e85ebdd28"
   };
+
  export default firebaseConfig;
